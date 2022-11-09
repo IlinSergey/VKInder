50f7634dd8d086935e3f066686bd6ca4fe4d6dc3
Sergey <ilin_sergey90@mail.ru>
2022-11-09 08:04:40 +0300

    Реализовал функцию отправки фото в сообщениях, теперь бот по команде идет в VK, берет рандомный id пользователя (проблему с периодически возврящяемым значением в виде None пока не удалось решить) и возвращает в сообщении ссылку на профиль и три фотографии.

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import pprint
 from random import randrange
 import config
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+import os
+
 from vk_agent import VkAgent
 
 token = config.vk_group_token
+user_token = config.vk_user_token
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
+vk_upload = vk_api.VkUpload(vk)
 
-vk_photo = VkAgent(config.vk_user_token)
+vk_user = VkAgent(config.vk_user_token)
 
 
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+
+def write_msg_with_photo(user_id):
+    resp = vk_upload.photo_messages(photos=['photo/1.jpg', 'photo/2.jpg', 'photo/3.jpg'])
+    for ph in resp:
+        vk.method('messages.send', {'user_id': user_id, 'attachment': f"photo{ph['owner_id']}_{ph['id']}", 'random_id': randrange(10 ** 7),})
+
 
 
 for event in longpoll.listen():
@@ -28,8 +40,8 @@ for event in longpoll.listen():
             elif request == "пока":
                 write_msg(event.user_id, "Пока((")
             elif request == 'Поехали':
-                link = vk_photo.get_photo()
-                list_photo = ['backup\\1.jpg']
-                write_msg(event.user_id, f'Ля vk.com/id{link}')
+                write_msg(event.user_id, f'Ля какая vk.com/id{vk_user.get_photo()}')
+                write_msg_with_photo(event.user_id)
+
             else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+                write_msg(evРеent.user_id, "Не поняла вашего ответа...")
--- vk_agent.py
+++ vk_agent.py
@@ -12,17 +12,14 @@ class VkAgent:
     def __init__(self, token: str):
         self.token = token
 
-
     def get_response(self, url, params):
         return requests.get(url, params=params).json()
 
-
-    def get_link(self, response, i=int):
+    def get_link(self, response, i):
 
         """Функция запрашивает у VK ссылку на скачивание фото"""
 
         return response['response']['items'][i]['sizes'][-1]['url']
-
 
     def find_users(self):
 
@@ -33,49 +30,54 @@ class VkAgent:
         params = {
             'access_token': self.token,
             'v': '5.131',
-            'sort': 1,
+            'sort': 0,
             'count': 1000,
             'status': 6,
             'sex': 1,
             'age_from': 25,
             'is_closed': False,
             'has_photo': 1,
-            'hometown': 'Санкт-Петербург'
+            'hometown': 'Москва'
         }
 
         response = self.get_response(url, params)
-        time.sleep(0.3)
+
 
         def select_id(response):
-            stop = len(response['response']['items'])
-            item = randrange(0, stop)
-            id = response['response']['items'][item]['id']
-            is_closed = response['response']['items'][item]['is_closed']
+            response_select_id = response
+            stop = len(response_select_id['response']['items'])
+            item = randrange(1, stop)
+            id_us = response_select_id['response']['items'][item]['id']
+            is_closed = response_select_id['response']['items'][item]['is_closed']
 
-            if is_closed == False:
-                if id is not None:
-                    return id
+            if not is_closed:
+                return id_us
             else:
                 select_id(response)
-        id = select_id(response)
-        if data_base.record_user(id):
-            return id
-        else:
-            select_id(response)
 
+        user_id = select_id(response)
+        if user_id is None:
+            select_id(response)
+        else:
+            if data_base.record_user(user_id):
+                print(user_id)
+                return user_id
+            else:
+                select_id(response)
 
     def get_photo(self):
 
         """Функция запрашиваает id пользователя, и если он удовлетворяет условиям,
-         скачивает три самых популярных (на основании лайков и комментариев) аватрки """
+         скачивает три самых популярных (на основании лайков и комментариев) фото профиля """
 
         url = 'https://api.vk.com/method/photos.get'
-        id = self.find_users()
-        if id is None:
+
+        user_id = self.find_users()
+        if user_id is None:
             self.find_users()
 
         params = {
-            'owner_id': id,
+            'owner_id': user_id,
             'album_id': 'profile',
             'extended': '1',
             'photo_sizes': '1',
@@ -86,6 +88,9 @@ class VkAgent:
 
         response = self.get_response(url, params)
         time.sleep(0.3)
+        """так-как не у всех профилей есть 3 фото и они не проходят проверку, отправляется повторный запрос,
+        возможны ситуации, когда бедет много запросов подряд, исмользую sleep дабы не попасть под ограничение
+         от VK в не более 3х запросов в секунду"""
         count_photo = len(response['response']['items'])
         if count_photo >= 3:
             count_for_name_photo = 1
@@ -105,18 +110,18 @@ class VkAgent:
 
             count_for_name_photo = 1
             for link in list_of_link:
-                f = open(f'backup\{count_for_name_photo}.jpg', 'wb')
+                f = open(rf'photo\{count_for_name_photo}.jpg', 'wb')
                 ufr = requests.get(link)
                 f.write(ufr.content)
                 f.close()
                 count_for_name_photo += 1
-            return 'Кажется готово'
+            return user_id
         else:
             self.get_photo()
 
 
 
 
-vk = VkAgent(config.vk_user_token)
-print(vk.find_users())
-vk.get_photo()
+
+#vk = VkAgent(config.vk_user_token)
+#vk.get_photo()
